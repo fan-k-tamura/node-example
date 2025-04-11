bef90f7b2994c1b668c947c3c9798aea423925a5
k_tamura <k_tamura@fancs.com>
2025-04-11 23:31:59 +0900

    first commit

---
+++ app/src/index.ts
@@ -0,0 +1,8 @@
+// A sample TypeScript function
+function greet(name: string): string {
+	const a = JSON.parse(name) as any;
+	console.log(a);
+	return `Hello, ${name}!`;
+}
+
+console.log(greet("World"));
